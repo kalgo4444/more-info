9dbf86c3ced7457857b9c5c5dd9ac626e3cdb362
kalgo4444 <abuxyziabd@gmail.com>
2025-07-18 15:21:22 +0500

    add

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './notf.scss'
 
 const NotFound = () => {
-  return <div>404</div>
+  const navigation = useNavigate()
+
+  return (
+    <section>
+      <div class="container1">
+        <h1>404</h1>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+        <div class="buttons">
+          <button onClick={() => navigation(-1)}>Go Back</button>
+          <button onClick={() => navigation('/')} class="home-button">
+            Home
+          </button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default NotFound
