519d5ccc5586ee1196c69860867df07aa91f0e7c
kalgo4444 <abuxyziabd@gmail.com>
2025-07-18 10:45:08 +0500

    add err

--- src/pages/info/Product.jsx
+++ src/pages/info/Product.jsx
@@ -12,50 +12,53 @@ const Info = () => {
   const { date, error, loading } = useFetch(`/products/${id}`)
 
   return (
-    <section className="product">
-      <div className="container">
-        <div className="product__wrapper">
-          <div className="product__left">
-            <h2 className="product__left-title">{date?.title}</h2>
-            <p className="product__left-category">{date?.category}</p>
-            <div className="product__left-img">
-              <img src={date?.image} alt="" />
-            </div>
-            <div className="product__left-rating">
-              {date?.rating.rate}
-              <div>
-                <RiStarSFill />
-                <RiStarSFill />
-                <RiStarSFill />
-                <RiStarSFill />
-                <RiStarSFill />
+    <>
+      {error && <p>Error</p>}
+      <section className="product">
+        <div className="container">
+          <div className="product__wrapper">
+            <div className="product__left">
+              <h2 className="product__left-title">{date?.title}</h2>
+              <p className="product__left-category">{date?.category}</p>
+              <div className="product__left-img">
+                <img src={date?.image} alt="" />
+              </div>
+              <div className="product__left-rating">
+                {date?.rating.rate}
+                <div>
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                  <RiStarSFill />
+                </div>
+              </div>
+              <div className="product__left-desc">
+                <p>{date?.description}</p>
               </div>
             </div>
-            <div className="product__left-desc">
-              <p>{date?.description}</p>
-            </div>
-          </div>
-          <div className="product__right">
-            <strong className="product__right-price">
-              <s>
-                <span>{date?.price} USD</span>
-              </s>{' '}
-              -- <span>1 USD</span>
-            </strong>
-            <p className="product__right-sale">Cheaper price</p>
-            <div className="product__right-tags">
-              <span>cheaper price</span>
-              <span>super price</span>
-            </div>
-            <button className="product__right-btn">Add to wash list</button>
-            <div className="product__right-count">
-              <p>{date?.rating.count} count of item can buy</p>
-              <p>This week 1512 people buy this</p>
+            <div className="product__right">
+              <strong className="product__right-price">
+                <s>
+                  <span>{date?.price} USD</span>
+                </s>{' '}
+                -- <span>1 USD</span>
+              </strong>
+              <p className="product__right-sale">Cheaper price</p>
+              <div className="product__right-tags">
+                <span>cheaper price</span>
+                <span>super price</span>
+              </div>
+              <button className="product__right-btn">Add to wash list</button>
+              <div className="product__right-count">
+                <p>{date?.rating.count} count of item can buy</p>
+                <p>This week 1512 people buy this</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
